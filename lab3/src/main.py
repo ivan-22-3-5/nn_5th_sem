579e99eedd21fff836d9af63b8cdc424823e20e0
Ivan Mihun <vsk76090@gmail.com>
2024-11-05 08:28:24 +0100

    feat(lab3): print prediction before training

--- lab3/src/main.py
+++ lab3/src/main.py
@@ -3,7 +3,13 @@ from src.network import Network
 
 def main():
     net = Network(2, 3, 2, 1)
+    print("before training")
+    print(net.predict([0.8, 0.5]))
+    print(net.predict([0.1, 0.3]))
+    print(net.predict([0.6, 0.1]))
     net.train(inputs=[[0.8, 0.5], [0.1, 0.3], [0.6, 0.1]], expected_outputs=[[0.45], [0.9], [0.4]], epochs=40000)
+
+    print("after training")
     print(net.predict([0.8, 0.5]))
     print(net.predict([0.1, 0.3]))
     print(net.predict([0.6, 0.1]))
